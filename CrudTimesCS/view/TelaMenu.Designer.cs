a8a08d590996a80efe36d7c5bbbe4b99c37ea57b
washington barreto <washington.perreiira@hotmail.com>
2022-05-11 12:01:45 -0300

    Telas de Cadastro

--- CrudTimesCS/view/TelaMenu.Designer.cs
+++ CrudTimesCS/view/TelaMenu.Designer.cs
@@ -67,8 +67,9 @@ namespace CrudTimesCS.view
             // sairToolStripMenuItem
             // 
             this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
-            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
             // 
             // jogadoresToolStripMenuItem
             // 
